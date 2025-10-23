db62627069423e628563d7d152d89e0fb9c9dba7
Rafael Manso <129555130+rafaeltmanso@users.noreply.github.com>
2025-10-23 10:13:13 -0300

    added configuration files for  Cypress cloud testing setup

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,13 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '6kucyi',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     experimentalStudio: true,
     baseUrl: 'http://localhost:3000',
-    
-
   },
-});
+})
